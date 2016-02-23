375e70fc90d836ca7d13e1f66fc551f427402401
tkmagesh <tkmagesh77@gmail.com>
2016-02-23 16:17:26 +0530

    Day-02 Complete

--- Day-02/Employee.js
+++ Day-02/Employee.js
@@ -1,10 +1,15 @@
 function Employee(id, name, salary){
     if (this.constructor.name !== 'Employee')
         return new Employee(id, name, salary);
-    this.id = id;
+    //this.id = id;
+    this.__id = id;
+
     this.name = name;
     this.salary = salary;
 }
 Employee.prototype.display = function(){
    console.log(this.id, this.name, this.salary);
+};
+Employee.prototype.getId = function(){
+  return this.__id;
 };
